8d78b0d2fc4b7b66279000921e67c6891f9a03c5
Varkey Josu <varkeyjosu2003@gmail.com>
2023-11-11 01:14:44 +0530

    signup auth

--- Frontend/Javascript/sign-in.js
+++ Frontend/Javascript/sign-in.js
@@ -1,9 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyC8DqWC16xVpRp4s05M5FkbLFKWzjIobkE",
   authDomain: "tasker-todo-app.firebaseapp.com",
@@ -15,3 +18,23 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+signup.addEventListner('click',(e)=>{
+    var email=document.getElementById('email').value;
+    var password=document.getElementById('password').value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    alert('Account Created');
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+    alert(errorMessage);
+  });
+});
