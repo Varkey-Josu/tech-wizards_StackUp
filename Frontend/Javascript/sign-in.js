2b4c053e239d3cc11d0c638b607a013c2d6dcd93
Varkey Josu <varkeyjosu2003@gmail.com>
2023-11-10 23:59:27 +0530

    Authentication

---
+++ Frontend/Javascript/sign-in.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC8DqWC16xVpRp4s05M5FkbLFKWzjIobkE",
+  authDomain: "tasker-todo-app.firebaseapp.com",
+  projectId: "tasker-todo-app",
+  storageBucket: "tasker-todo-app.appspot.com",
+  messagingSenderId: "481453018191",
+  appId: "1:481453018191:web:0ca6a1a9f800cac9222e3a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
